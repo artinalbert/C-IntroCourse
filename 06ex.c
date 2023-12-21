19c688c986a10319472cab47eba72fda7177aae3
Amirhossein Dabaghmanesh <58168246+artinalbert@users.noreply.github.com>
2023-12-21 16:52:09 +0100

    initial solution print function to debug

--- 06ex.c
+++ 06ex.c
@@ -14,6 +14,30 @@ cc -std=c11 -g -Wall -Werror 06ex_test.c -o 06ex_test.o -lm && valgrind --leak-c
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+void printArray(uint8_t *arr, size_t len)
+{
+    const char *s2 = format_uint8_array(arr, len);
+    printf("%s",s2);
+    free((char *)s2);
+}
+uint8_t *manipulator(uint8_t *arr, size_t len)
+{
+    // create an array with the sae size
+    // stack(bound to the scope) vs  - heap(shared - with malloc - should be freed):
+
+    uint8_t *newArr = malloc(len * sizeof(uint8_t));
+
+    if (newArr == NULL)
+    {
+        // Handle memory allocation failure
+    }
+    newArr[len - 1] = arr[0];
+    for (size_t i = 0; i <= (len - 1); i++)
+    {
+        newArr[i] = arr[i + 1];
+    }
+    return newArr;
+}
 
 /*
 Aufgabe 1:
@@ -22,10 +46,14 @@ Nutzen Sie die `visualizer_append_array` Funktion damit die Tests durchlaufen.
 
 Tipp 1: Die erste Zeile im erzeugten Bild stellt das Eingabearray dar.
 */
-void warmup(Visualizer *v, uint8_t *arr, size_t len) {
-    
+void warmup(Visualizer *v, uint8_t *arr, size_t len)
+{
+    visualizer_append_array(v, arr);
+    arr = manipulator(arr, len);
+    printArray(arr,len);
+    visualizer_append_array(v, arr);
+    manipulator(arr, len);
 }
-
 /*
 Aufgabe 2:
 Bringen Sie die Tests zum durchlaufen.
@@ -34,6 +62,6 @@ Tipp 1: Die erste Zeile im erzeugten Bild stellt das Eingabearray dar.
 Tipp 2: Es handelt sich um eine Abwandlung von iterativem Mergesort.
 Tipp 3: `len` ist immer eine Dreierpotenz, damit Sie sich nicht mit Rundungsdetails herumschlagen brauchen.
 */
-void sort_it(Visualizer *v, uint8_t *arr, size_t len) {
-    
+void sort_it(Visualizer *v, uint8_t *arr, size_t len)
+{
 }
